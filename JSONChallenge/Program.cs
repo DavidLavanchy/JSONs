83e4961ae011b195771f644131facc45c4d1fbbf
David Lavanchy <dplavanchy@gmail.com>
2021-07-27 21:05:24 -0400

    first commit

---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.FirstName = "David";
+            person.LastName = "Lavanchy";
+            person.DOB = new DateTime(1998, 04, 07);
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
